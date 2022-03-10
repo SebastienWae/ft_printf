b8213e608920fff38f426f4eb7e34e95a4837eba
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-03-10 13:10:49 +0100

    fix clangd config

--- test/test.c
+++ test/test.c
@@ -1,7 +1,8 @@
+#include <ft_printf.h>
+
 #include <criterion/criterion.h>
 #include <criterion/new/assert.h>
 
-#include <ft_printf.h>
 
 Test(ft_printf, eq) {
     cr_assert(eq(i64, ft_printf("test"), 1));
